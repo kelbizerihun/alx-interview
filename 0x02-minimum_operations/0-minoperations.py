a752b90ba6262fd39a8ed51399f7c9df54533b94
kelbizerihun <kelbizerihun3080@gmail.com>
2022-06-30 09:04:38 +0000

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,11 +1,11 @@
-0r/bin/python3
-'''Minimum Operations challenge'''
+#!/usr/bin/python3
+'''Minimum Operations python3 challenge'''
 
 
 def minOperations(n):
     '''calculates the fewest number of
     operations needed to result in exactly n H
-    characters in the file.
+    characters in this file.
     Returns:
         Integer : if n is impossible to achieve, return 0
     '''
